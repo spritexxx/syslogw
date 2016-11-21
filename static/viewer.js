42e7824ac8ba11ce5bdcfc028519485f8f9be132
Simon Esprit <simon.esprit@gmail.com>
2016-11-21 20:58:57 +0100

    viewer page should display received syslog messages
    Basic implementation which autoscrolls the whole page.
    Future implementation should fix the table header & possible viewer controls
    in a fixed position.

--- static/viewer.js
+++ static/viewer.js
@@ -10,27 +10,30 @@ function init() {
     window.init()
 }
 
+function isEmpty(str) {
+    return (!str || 0 === str.length);
+}
+
 app.controller('MessagesController', ['$scope', '$window', function ($scope, $window){
     $scope.socket = null;
     $scope.socket_is_open = false;
     $scope.messages = [];
-    $scope.messages.push("connecting to syslogc");
 
     var init = function() {
 	    socket = new WebSocket("ws://127.0.0.1:9494");
 
 	    socket.onopen = function() {
-		    console.log("Connected!");
-		    isopen = true;
+		    $scope.socket_is_open = true;
+		    $scope.socket = socket
 
 		    // example if the message were a string
-		    $scope.messages.push("connected to syslogc");
+		    $scope.messages.push({severity: "notice", msg: "Connected to syslogc"});
 		    $scope.$apply();
 	    }
 
 	    socket.onmessage = function(e) {
 		    if (typeof e.data == "string") {
-			    console.log("Message received: " + e.data);
+			    $scope.handleMessage(e.data)
 		    } else {
 			    var arr = new Uint8Array(e.data);
 			    var hex = '';
@@ -42,12 +45,30 @@ app.controller('MessagesController', ['$scope', '$window', function ($scope, $wi
 	    }
 
 	    socket.onclose = function(e) {
-		    console.log("Connection closed.");
-		    socket = null;
-		    isopen = false;
+		    $scope.socket = null;
+		    $scope.socket_is_open = false;
+
+		    $scope.messages.push({severity: "notice", msg: "Disconnected from syslogc"});
+		    $scope.$apply();
 	    }
 
 	    $scope.socket = socket
+
+	    $scope.handleMessage = function(string) {
+			    message = JSON.parse(string);
+
+			    if(isEmpty(message.msg)) {
+			        console.log("not logging empty message");
+			        return;
+			    }
+
+			    $scope.messages.push(message);
+			    $scope.$apply();
+
+			    // scroll to bottom
+			    // TODO make cleaner implementation where the table header and controls stay on top...
+			    window.scrollTo(0, document.body.scrollHeight);
+	    }
 	}
 
 	init();
